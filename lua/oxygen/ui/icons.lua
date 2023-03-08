e3df35820079c5f41880472e299e05641f1ca7a2
Can Kolay <cankolay3499@gmail.com>
2023-03-08 23:43:01 +0300

    chore: add new one icon

--- lua/oxygen/ui/icons.lua
+++ lua/oxygen/ui/icons.lua
@@ -16,6 +16,7 @@ ui.icons = {
   sleep = '鈴',
   tick = '',
   empty = '◌',
+  color_palette = '󱥚',
   nvimtree = {
     glyphs = {
       default = '',
@@ -46,17 +47,14 @@ ui.icons = {
       left = '',
       right = ' ',
     },
-
     round = {
       left = '',
       right = '',
     },
-
     block = {
       left = '█',
       right = '█',
     },
-
     arrow = {
       left = '',
       right = '',
@@ -106,137 +104,110 @@ ui.icons = {
       icon = '',
       name = 'Default',
     },
-
     c = {
       icon = '',
       name = 'c',
     },
-
     css = {
       icon = '',
       name = 'css',
     },
-
     deb = {
       icon = '',
       name = 'deb',
     },
-
     Dockerfile = {
       icon = '',
       name = 'Dockerfile',
     },
-
     html = {
       icon = '',
       name = 'html',
     },
-
     jpeg = {
       icon = '',
       name = 'jpeg',
     },
-
     jpg = {
       icon = '',
       name = 'jpg',
     },
-
     js = {
       icon = '',
       name = 'js',
     },
-
     kt = {
       icon = '󱈙',
       name = 'kt',
     },
-
     lock = {
       icon = '',
       name = 'lock',
     },
-
     lua = {
       icon = '',
       name = 'lua',
     },
-
     mp3 = {
       icon = '',
       name = 'mp3',
     },
-
     mp4 = {
       icon = '',
       name = 'mp4',
     },
-
     out = {
       icon = '',
       name = 'out',
     },
-
     png = {
       icon = '',
       name = 'png',
     },
-
     py = {
       icon = '',
       name = 'py',
     },
-
     ['robots.txt'] = {
       icon = 'ﮧ',
       name = 'robots',
     },
-
     toml = {
       icon = '',
       name = 'toml',
     },
-
     ts = {
       icon = 'ﯤ',
       name = 'ts',
     },
-
     ttf = {
       icon = '',
       name = 'TrueTypeFont',
     },
-
     rb = {
       icon = '',
       name = 'rb',
     },
-
     rpm = {
       icon = '',
       name = 'rpm',
     },
-
     vue = {
       icon = '﵂',
       name = 'vue',
     },
-
     woff = {
       icon = '',
       name = 'WebOpenFontFormat',
     },
-
     woff2 = {
       icon = '',
       name = 'WebOpenFontFormat2',
     },
-
     xz = {
       icon = '',
       name = 'xz',
     },
-
     zip = {
       icon = '',
       name = 'zip',
